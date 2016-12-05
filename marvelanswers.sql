da35a6f2e9f1bff5ec83e123920a8993dc7ece95
Amy Robinson <amskyrobinson@gmail.com>
2016-12-05 23:47:30 +0000

    pushing up hw

---
+++ marvelanswers.sql
@@ -0,0 +1,24 @@
+-- SELECT * FROM movies;
+
+-- SELECT name FROM people;
+
+-- SELECT * FROM people WHERE name = 'Amy Robinson';
+
+
+-- DELETE FROM movies where title = 'Batman Begins';
+-- SELECT title FROM movies;
+
+-- INSERT INTO people (name) VALUES ('Matthew');
+-- SELECT name FROM people;
+
+-- DELETE FROM people WHERE name = 'Craig Morton';
+-- SELECT name FROM people;
+
+-- UPDATE people SET name = 'Andy Guppy' where name = 'Andrew Guppy';
+-- SELECT name FROM people;
+
+-- INSERT INTO movies (title) VALUES ('Guardians of the Galaxy 2!');
+-- SELECT title from movies;
+
+-- UPDATE movies SET show_time = '21.30' where title = 'Guardians of the Galaxy';
+-- SELECT show_time, title from movies;
